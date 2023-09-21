77bd3b243aec851a2069dff2ecc5ce651919c517
YESURAJUKOLLATI <21pa1a0579@vishnu.edu.in>
2023-09-21 07:00:16 +0530

    Added express to app

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hi");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! from raju')
+})
+app.get('/hello', (req, res) => {
+    res.send('Hello World! from')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
